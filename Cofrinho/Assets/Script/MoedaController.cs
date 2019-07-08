bd83b4bdf7e5195057435818f155b5fce7e98f35
NaejNunes <jeannobreganunes@hotmail.com>
2019-07-08 00:28:19 -0300

    Iniciando o projeto

---
+++ Cofrinho/Assets/Script/MoedaController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoedaController : MonoBehaviour
+{   
+    bool moedaEscolhida;
+
+    public Transform[] wayPoints;
+
+    int cur;
+
+    public float speed;
+
+    private Animator animacao;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animacao = GetComponent<Animator>();
+
+        moedaEscolhida = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moedaEscolhida == true)
+        {
+            if (transform.position != wayPoints[cur].position)
+            {
+                Vector2 p = Vector2.MoveTowards(transform.position, wayPoints[cur].position, speed);
+                GetComponent<Rigidbody2D>().MovePosition(p);
+            }
+            else
+            {
+                cur = (cur + 1) % wayPoints.Length;
+            }
+
+            Vector2 dir = wayPoints[cur].position - transform.position;
+            GetComponent<Animator>().SetFloat("DirX", dir.x);
+            GetComponent<Animator>().SetFloat("DirY", dir.y);
+        }
+    }
+
+    public void OnMouseDown()
+    {        
+            moedaEscolhida = true;              
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DestinoMoeda"))
+        {
+            animacao.SetBool("DestroirMoeda", true);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Cofrinho/Assets/Script/ToqueNaTela.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToqueNaTela : MonoBehaviour {
+
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+
+        cam = GetComponent<Camera>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Cancel")) {
+
+			Application.Quit ();
+
+		}
+
+            if (Input.touchCount > 0) {
+
+                foreach (Touch touch in Input.touches) {
+
+                    Ray ray = cam.ScreenPointToRay(touch.position);
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit)) {
+
+                        GameObject Objeto = hit.transform.gameObject;
+
+                        switch (touch.phase) {
+
+                            case TouchPhase.Began:
+                                Objeto.SendMessage("Began", SendMessageOptions.DontRequireReceiver);
+                                break;
+
+                            case TouchPhase.Ended:
+                                Objeto.SendMessage("Ended", SendMessageOptions.DontRequireReceiver);
+                                break;
+
+                            case TouchPhase.Stationary:
+                                Objeto.SendMessage("Stationary", SendMessageOptions.DontRequireReceiver);
+                                break;
+
+                            case TouchPhase.Moved:
+                                Objeto.SendMessage("Moved", SendMessageOptions.DontRequireReceiver);
+                                break;
+
+                            case TouchPhase.Canceled:
+                                Objeto.SendMessage("Canceled", SendMessageOptions.DontRequireReceiver);
+                                break;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+	}
+
